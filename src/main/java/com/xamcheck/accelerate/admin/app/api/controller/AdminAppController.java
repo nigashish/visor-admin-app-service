4d9e230a361a6725420f41f8ed3ae72cfb454824
nigashish <ashishn@eupheus.in>
2023-05-16 13:30:42 +0530

    Add model file into a seperate service

--- src/main/java/com/xamcheck/accelerate/admin/app/api/controller/AdminAppController.java
+++ src/main/java/com/xamcheck/accelerate/admin/app/api/controller/AdminAppController.java
@@ -1,28 +1,22 @@
 package com.xamcheck.accelerate.admin.app.api.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.xamcheck.accelerate.admin.app.api.dto.ResponceDto;
-import com.xamcheck.accelerate.admin.app.api.services.AdminAppService;
-
 
 
 @RestController
 @RequestMapping("/admin")
 public class AdminAppController {
-	@Autowired AdminAppService adminAppService;	
 
 	@GetMapping("/test")
 	public ResponceDto test() {
-		ResponceDto responceDto=new ResponceDto();
-		responceDto =adminAppService.checkServicecall();
+		ResponceDto responceDto = new ResponceDto();
 		responceDto.setMessage("Just test the Admin App Ends Points");
 		return responceDto;
-		
+
 	}
 
 }
--- src/main/java/com/xamcheck/accelerate/admin/app/api/repository/AdminAppRespository.java
+++
@@ -1,8 +0,0 @@
-package com.xamcheck.accelerate.admin.app.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface AdminAppRespository {
-
-}
